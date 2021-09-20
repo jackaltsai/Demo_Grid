96e714748ee92ed61847770a135605e7293cb4e1
jackaltsai <hata.s520@gmail.com>
2021-09-20 21:40:57 +0800

    格子狀照片牆(grid)

--- Demo_Grid/ViewController.swift
+++ Demo_Grid/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.collectionViewLayout = generateLayout()
     }
-
-
+    
+    /* 格子狀照片牆(grid) */
+    // item & group 的高度傳入 estimated，由 auto layout 自動計算高度
+    // image view 比例設為 0.7，priority 要設成 999，否則會有 auto layout breaking constraint 的警告訊息
+    
+    func generateLayout() -> UICollectionViewLayout {
+        let space: Double = 10
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(100))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(100))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        group.interItemSpacing = .fixed(space)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = space
+        section.contentInsets = NSDirectionalEdgeInsets(top: space, leading: space, bottom: space, trailing: space)
+        return UICollectionViewCompositionalLayout(section: section)
+    }
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(CollectionViewCell.self)", for: indexPath) as! CollectionViewCell
+        cell.imageView.image = UIImage(named: "Image\(indexPath.item + 1)")
+        cell.label.text = "Image\(indexPath.item + 1)"
+        return cell
+    }
+    
+    
+    
+    
+}
